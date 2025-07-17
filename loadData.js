d99c9bc0b03ce5bb154196571bcd5743c44593cc
emlifrfr <emilklum.ekes@gmail.com>
2025-07-17 16:56:03 +0200

    updated ui

--- loadData.js
+++ loadData.js
@@ -2,10 +2,11 @@ fetch(
         "https://opensheet.elk.sh/1N99ZxvqERsNDrpHDudUraMskA8Pcr9jR7Wh5tXmBnbA/main"
         ).then(response=>response.json())
         
-        .then(data=>{ editSite(data[0].count) })
+        .then(data=>{ editSite(data[0].count, data[0].lastPic) })
 
-        function editSite(count){
+        function editSite(count, last){
             let mod10 = count % 10
+            let tens = count - mod10
             let next
             
                 if (mod10 < 3){next = 3;barLen=3;
@@ -14,13 +15,16 @@ fetch(
                 }else if  (mod10 < 10){next = 10;barLen=3;}
 
                 let barProg = barLen - (next - mod10);
+                let missing = next - mod10;
             
-
+                next += tens
+                
+            //<label id="label1" for="bar">${next-barLen}</label>
 
             document.body.innerHTML = `
             
             <div class="header-row">
-                <h1>Emli&Lucy GbR</h1>
+                <h1>☭ Emli&Lucy GbR</h1>
                 <br>
                 
             </div>
@@ -28,11 +32,13 @@ fetch(
             <h2>Donations: ${count}</h2><br>
                 <h4>Next Goal:</h3><br>
                 <h2>${next} gifted Monsters</h2><br>
+                <h4>${next - missing}/${next}</h4><br>
                     <div class="barDiv">
-                    <label id="label1" for="bar">0</label><progress id="bar" value="${barProg}" max="${barLen}"></progress><label id="label2" for="bar">3</label>
+                    
+                    <progress id="bar" value="${barProg}" max="${barLen}"></progress>
                     </div>
                     <br>
-                
+                    <h4>${missing} missing!</h3><br>
            
             <a href="whatsapp://send?text=*Coupon für 1 Monster <3*" data-action="share/whatsapp/share"><h1>DONATE :3</h1></a>
 
